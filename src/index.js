a1ab2e503e344dd4619ebec804ec0e3b76840209
“✨Anakraken✨” <anakrakennn@gmail.com>
2023-11-24 13:23:26 -0600

    remove dotenv from index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import dotenv from 'dotenv';
-import path from 'path';
 import app from './app.js';
 import { connectDB } from './db.js';
-import { __dirname } from '../env_path.js';
-
-dotenv.config({ path: path.resolve(__dirname + '/.env') });
 
 //Settings
 connectDB();
